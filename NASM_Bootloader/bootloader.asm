413791e7974a3122870efb0603a9c77ee9519614
Alpha Emmanuel <65494407+ealpha072@users.noreply.github.com>
2023-04-28 21:30:23 +0300

    Update bootloader.asm

--- NASM_Bootloader/bootloader.asm
+++ NASM_Bootloader/bootloader.asm
@@ -3,9 +3,9 @@
 ;print string to std output "Hi"
 mov ah, 0x0E.  ;Teletype 
 mov al, 'H'   ;Move byte
-int 0x10
+int 0x10     ;Call interupt
 mov al, 'i'
-int 0x10
+int 0x10     ;Call interupt
 
 times 510 - ($-$$) db 0
 dw 0xAA55

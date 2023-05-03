142a93441304748a61e2986b3e2b79ac15d8e265
ealpha072 <ealpha072@gmail.com>
2023-05-03 14:00:00 +0300

    Changed commits

--- Assembly_language/hello_world.asm
+++ Assembly_language/hello_world.asm
@@ -2,7 +2,7 @@
 ; Compile with: nasm -f elf helloworld.asm
 ; Link with (64 bit systems require elf_i386 option): ld -m elf_i386 helloworld.o -o helloworld
 ; Run with: ./helloworld
- 
+; this is to be run on a linux 
 
 SECTION .data
 	hello db 'Hello world!', 10 ;sets new line charcyer after msg, ensures next msg is printed in new line
@@ -12,14 +12,14 @@ SECTION .text
 
 _start:
 	;write hello world to standard output
-	mov eax, 4 ;system call for write
-	mov ebx, 1 ;file descriptor for std output
-	mov ecx, hello ;load message to register
-	mov edx, 13 ;length to read and write to std output in bytes
-	int 0x80 ;call kernel
+	mov eax, 4 		;system call for write
+	mov ebx, 1 		;file descriptor for std output
+	mov ecx, hello  ;load message to register
+	mov edx, 13     ;length to read and write to std output in bytes
+	int 0x80        ;call kernel
 
 	;write new line
-	mov eax, 4 ;same as above
+	mov eax, 4      ;same as above
 	mov ebx, 1
 	mov ecx, 0x0a
 	mov edx, 1

f6fc60a0641a63fa6831b747e8d42f692cb3a44c
Alpha Emmanuel <65494407+ealpha072@users.noreply.github.com>
2023-05-08 21:56:58 +0300

    Comments

--- Assembly_language/hello_world.asm
+++ Assembly_language/hello_world.asm
@@ -5,21 +5,21 @@
 ; this is to be run on a linux 
 
 SECTION .data
-	hello db 'Hello world!', 10 ;sets new line charcyer after msg, ensures next msg is printed in new line
+	hello db 'Hello world!', 10 		;sets new line charcyer after msg, ensures next msg is printed in new line
 
 SECTION .text
 	global _start
 
 _start:
 	;write hello world to standard output
-	mov eax, 4 		;system call for write
-	mov ebx, 1 		;file descriptor for std output
-	mov ecx, hello  ;load message to register
-	mov edx, 13     ;length to read and write to std output in bytes
-	int 0x80        ;call kernel
+	mov eax, 4 				;system call for write
+	mov ebx, 1 				;file descriptor for std output
+	mov ecx, hello  			;load message to register
+	mov edx, 13     			;length to read and write to std output in bytes
+	int 0x80        			;call kernel
 
 	;write new line
-	mov eax, 4      ;same as above
+	mov eax, 4      			;same as above
 	mov ebx, 1
 	mov ecx, 0x0a
 	mov edx, 1

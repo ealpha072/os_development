0c53b22f43a16db74194d363da58dd5575deb625
ealpha072 <ealpha072@gmail.com>
2023-05-10 21:47:24 +0300

    Print 9 stars

--- Assembly_language/hello_world_loop.asm
+++ Assembly_language/hello_world_loop.asm
@@ -5,4 +5,28 @@
 
 
 section .data
+    msg db "Display 9 stars", 0xa
+    len equ $ - msg
+    stars times 9 db '*'
+
+section .text
+    global _start
+
+_start:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg
+    mov edx, len
+    int 0x80
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, stars
+    mov edx, 9
+    int 0x80
+
+    ;exit
+    mov eax, 1
+    int 0x80
+
 
